b01100eb0239c6283f1512418130a41a06feb94c
heaven7 <532278976@qq.com>
2022-12-29 13:08:44 +0800

    add CMakeLists.txt

---
+++ etc/everywhere/gen_c.sh
@@ -0,0 +1,3 @@
+java -jar antlr-3.4-complete-no-antlrv2.jar ./ExprCppTree.g
+# 看一下文件，应该有这些
+#ExprCppTree.g  ExprCppTreeLexer.c  ExprCppTreeLexer.h  ExprCppTreeParser.c  ExprCppTreeParser.h  ExprCppTree.tokens
